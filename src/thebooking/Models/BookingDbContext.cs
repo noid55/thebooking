a8f3937a800df5e6ea8eaac0ff779e63ed80e1ea
ilham <ilhamh7@hotmail.com>
2026-09-23 16:06:47 +0200

    migration delen gjort

--- src/thebooking/Models/BookingDbContext.cs
+++ src/thebooking/Models/BookingDbContext.cs
@@ -6,7 +6,7 @@ public class BookingDbContext : DbContext
 {
     public BookingDbContext(DbContextOptions<BookingDbContext> options) : base(options)
     {
-        Database.EnsureCreated();
+       // Database.EnsureCreated();
     }
     public DbSet<Room> Rooms { get; set; }
 }
